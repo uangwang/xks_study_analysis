704641bcb848dfeb594d1f4632e1736fb7ee53bf
uangwang <202119121729@stu.cdut.edu.cn>
2024-03-22 01:37:48 +0800

    运行正常但卡住

--- study_server/src/main/java/com/xks/App.java
+++ study_server/src/main/java/com/xks/App.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        System.out.println("start...");
         SpringApplication.run(App.class);
+        System.out.println("end...");
     }
 }
