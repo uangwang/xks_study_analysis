381faee21bf774689af3145eb3b7e26129c96d8b
uangwang <202119121729@stu.cdut.edu.cn>
2024-03-22 09:53:21 +0800

    成功引入echart

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import * as echarts from 'echarts';
+import 'echarts-gl';
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
--- client/src/views/Charts.vue
+++ client/src/views/Charts.vue
@@ -1,15 +1,52 @@
 <template>
-  <div >
-    11223344
+  <div>
+    <div id="main"></div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "Charts-view"
+  name: "Charts-view",
+  methods:{
+    drawChart() {
+      let myEchart = this.$echarts.init(document.getElementById("main"));
+      let option = {
+        title: {
+          text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+      myEchart.setOption(option);
+    }
+  },
+  mounted() {
+    this.drawChart();
+  }
+
 }
+
+
+
 </script>
 
-<style scoped>
 
+<style scoped>
+#main {
+  width: 600px;
+  height:400px;
+  margin: auto;
+  margin-top: 100px
+}
 </style>
